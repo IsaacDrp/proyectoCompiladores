8a96a759f94c267ab842c2aaa2a52851b4410758
IsaacDrp <isaacdp09@gmail.com>
2024-06-05 15:36:09 -0600

    Arreglados errores de diseño

--- src/main/java/fes/aragon/modelo/Fondo.java
+++ src/main/java/fes/aragon/modelo/Fondo.java
@@ -17,6 +17,8 @@ import javafx.stage.Stage;
 public class Fondo extends ComponentesJuego {
 	private int yy = 0;
 	private int xx = 0;
+	private int repeticiones = 1;
+	private int bloqueRepetir;
 	private Image arribaImg;
 	private Image abajoImg;
 	private Image derechaImg;
@@ -237,27 +239,72 @@ public class Fondo extends ComponentesJuego {
 			case "coloca":
 				x = 55;
 				y = 55;
-				xx = Integer.parseInt(datos[1]);
-				yy = Integer.parseInt(datos[2]);
-				xx = (x + (ancho + 10) * (xx - 1));
-				yy = (y + (alto + 10) * (yy - 1));
-				this.comando = "coloca";
+				int datosX = Integer.parseInt(datos[1]);
+				int datosY = Integer.parseInt(datos[2]);
+
+				System.out.println(datosX + " " + datosY);
+				if ((datosX < 11 && datosX > 0) || (datosY < 11 && datosY > 0)) {
+					xx = datosX;
+					yy = datosY;
+					xx = (x + (ancho + 10) * (xx - 1));
+					yy = (y + (alto + 10) * (yy - 1));
+					this.comando = "coloca";
+				}
+				else {
+					System.out.println("No salgas de los bordes\n colocado por defecto 1 1");
+
+					xx = x;
+					yy = y;
+				}
 				break;
 			case "mover":
+				int xxtmp;
+				int yytmp;
+				boolean invalidMove = false;
 				moverCuadros = Integer.parseInt(datos[1]);
 				if (arriba) {
-					yy = (y - (alto + 10) * moverCuadros);
+					yytmp = (y - (alto + 10) * moverCuadros);
+					if(yytmp > 55)
+						yy = yytmp;
+					else
+						invalidMove = true;
+
 				}
 				if (abajo) {
-					yy = (y + (alto + 10) * moverCuadros);
+					yytmp = (y + (alto + 10) * moverCuadros);
+					if (yytmp < 555)
+						yy = yytmp;
+					else
+						invalidMove = true;
 				}
 				if (izquierda) {
-					xx = (x - (ancho + 10) * moverCuadros);
+					xxtmp = (x - (ancho + 10) * moverCuadros);
+					if (xxtmp > 5)
+						xx = xxtmp;
+					else
+						invalidMove = true;
 				}
 				if (derecha) {
-					xx = (x + (ancho + 10) * moverCuadros);
+					xxtmp = (x + (ancho + 10) * moverCuadros);
+					if (xxtmp < 555)
+						xx = xxtmp;
+					else
+						invalidMove = true;
 				}
+				if (invalidMove)
+					System.out.println("Saldrias de los limites\nno avanzare");
 				this.comando = "mover";
+				break;
+			case "repetir":
+				bloqueRepetir = indice;
+				repeticiones = Integer.parseInt(datos[1]);
+				break;
+			case "fin_repetir":
+				if(repeticiones > 1) {
+					System.out.println("repeticion " + repeticiones);
+					indice = bloqueRepetir;
+					repeticiones--;
+				}
 				break;
 
 			default:
@@ -265,11 +312,10 @@ public class Fondo extends ComponentesJuego {
 			}
 
 		} else {
-			System.out.println("limite");
+			System.out.println("fin");
 			this.iniciar = false;
 			this.indice = 1;
 		}
-
 	}
 
 	private void abrirArchivo() throws IOException, ClassNotFoundException {
@@ -284,10 +330,11 @@ public class Fondo extends ComponentesJuego {
 		//ParserJuego parser = new ParserJuego();
 		//parser.setRuta(ruta.getAbsolutePath());
 
-		//comandos = parser.task();
+		//Arraylist de reporte del parser
 
-		//esto de abajo se fa pal fri fayer
-		if (ruta != null) {
+		ArrayList<String> reporte = parser.task();
+
+		if (ruta != null /* && reporte.size().equals("aprobado") */) {
 			FileReader fr = new FileReader(ruta);
 			BufferedReader buff = new BufferedReader(fr);
 			String cad;
